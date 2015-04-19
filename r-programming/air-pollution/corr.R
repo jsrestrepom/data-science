735d0b7a5a6a046f0e6f5d49acacb366d39a9620
jsrestrepom <santyrestrepom@gmail.com>
2015-04-19 14:53:50 -0500

    Adding corr function implementation

--- r-programming/air-pollution/corr.R
+++ r-programming/air-pollution/corr.R
@@ -1,11 +1,26 @@
 corr <- function(directory, threshold = 0) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
+  result <- NULL
+  observations <- complete(directory)
+  for(observation in observations$id) {
+    if(observations$nobs[observation] > threshold) {
+      pollutionData <- readLectureOfMonitor(observation, directory)
+      correlation <- cor(pollutionData$sulfate, pollutionData$nitrate, use = "complete.obs")
+      result <- c(result, correlation)
+    }
+  }
+  result
+}
 
-  ## 'threshold' is a numeric vector of length 1 indicating the
-  ## number of completely observed observations (on all
-  ## variables) required to compute the correlation between
-  ## nitrate and sulfate; the default is 0
-
-  ## Return a numeric vector of correlations
+readLectureOfMonitor <- function(monitorID, directory) {
+  zeroBefore <- if(monitorID < 10) {
+    "00"
+  }
+  else if(monitorID < 100) {
+    "0"
+  }
+  else {
+    ""
+  }
+  filename <- paste(zeroBefore, monitorID, ".csv", sep = "")
+  read.csv(paste(directory, filename, sep = "/"), comment.char = "")
 }
