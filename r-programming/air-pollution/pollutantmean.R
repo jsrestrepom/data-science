04bf04d1a03bf5c7ab83447dde169133fc49c7eb
jsrestrepom <santyrestrepom@gmail.com>
2015-04-18 23:06:36 -0500

    Adding pollutantmean function implementation

--- r-programming/air-pollution/pollutantmean.R
+++ r-programming/air-pollution/pollutantmean.R
@@ -1,14 +1,24 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  
-  ## 'pollutant' is a character vector of length 1 indicating
-  ## the name of the pollutant for which we will calculate the
-  ## mean; either "sulfate" or "nitrate".
-  
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-  
-  ## Return the mean of the pollutant across all monitors list
-  ## in the 'id' vector (ignoring NA values)
+  totalPollutant <- NULL
+  for(monitor in id) {
+    pollutionData <- readLectureFromMonitor(monitor, directory)
+    pollutantSubset <- pollutionData[pollutant]
+    cleanPollutantSubset <- pollutantSubset[!is.na(pollutantSubset)]
+    totalPollutant <- c(totalPollutant, cleanPollutantSubset)
+  }
+  mean(totalPollutant)
+}
+
+readLectureFromMonitor <- function(monitorID, directory) {
+  zeroBefore <- if(monitorID < 10) {
+    "00"
+  }
+  else if(monitorID < 100) {
+    "0"
+  }
+  else {
+    ""
+  }
+  filename <- paste(zeroBefore, monitorID, ".csv", sep = "")
+  read.csv(paste(directory, filename, sep = "/"), comment.char = "")
 }
