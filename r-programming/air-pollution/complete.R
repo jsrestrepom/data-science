badf4b5de042368da1003922ec1173ae972c9401
jsrestrepom <santyrestrepom@gmail.com>
2015-04-19 13:27:36 -0500

    Refactoring an internal function in order to optimize the global execution time of complete function, using the complete.cases(...) directive

--- r-programming/air-pollution/complete.R
+++ r-programming/air-pollution/complete.R
@@ -2,7 +2,7 @@ complete <- function(directory, id = 1:332) {
   groupOfID <- NULL
   groupOfNobs <- NULL
   for(monitor in id) {
-    numberObservedCases <- completeCasesFromAllObserved(readLectureOfMonitor(monitor, directory))
+    numberObservedCases <- numberOfCompleteCasesFrom(readLectureOfMonitor(monitor, directory))
     if(numberObservedCases > 0) {
       groupOfID <- c(groupOfID, monitor)
       groupOfNobs <- c(groupOfNobs, numberObservedCases)
@@ -11,18 +11,10 @@ complete <- function(directory, id = 1:332) {
   data.frame(id = groupOfID, nobs = groupOfNobs)
 }
 
-completeCasesFromAllObserved <- function(observedCases) {
-  count <- 0
-  for(i in row.names(observedCases)) {
-    if(isComplete(observedCases[i, ])) {
-      count <- count + 1
-    }
-  }
-  count
-}
-
-isComplete <- function(observedCase) {
-  !(is.na(observedCase$sulfate) | is.na(observedCase$nitrate))
+numberOfCompleteCasesFrom <- function(observedCases) {
+  isCompleteCondition <- complete.cases(observedCases$nitrate, observedCases$sulfate)
+  completeCases <- observedCases$ID[isCompleteCondition]
+  length(completeCases)
 }
 
 readLectureOfMonitor <- function(monitorID, directory) {
