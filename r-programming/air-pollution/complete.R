42cc2ccaf7527b1258ec9b2f8219cb7e058ab853
jsrestrepom <santyrestrepom@gmail.com>
2015-04-19 11:38:07 -0500

    Adding complete function implementation

--- r-programming/air-pollution/complete.R
+++ r-programming/air-pollution/complete.R
@@ -1,15 +1,36 @@
 complete <- function(directory, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-  
-  ## Return a data frame of the form:
-  ## id nobs
-  ## 1  117
-  ## 2  1041
-  ## ...
-  ## where 'id' is the monitor ID number and 'nobs' is the
-  ## number of complete cases
+  listOfID <- NULL
+  listOfNobs <- NULL
+  for(monitor in id) {
+    pollutionData <- readLectureFromMonitor(monitor, directory)
+    count <- 0
+    for(i in row.names(pollutionData)) {
+      if(isComplete(pollutionData[i, ])) {
+        count <- count + 1
+      }
+    }
+    if(count > 0) {
+      listOfID <- c(listOfID, monitor)
+      listOfNobs <- c(listOfNobs, count)
+    }
+  }
+  data.frame(id = listOfID, nobs = listOfNobs)
+}
+
+isComplete <- function(observedCase) {
+  !is.na(observedCase$sulfate) & !is.na(observedCase$nitrate)
+}
+
+readLectureFromMonitor <- function(monitorID, directory) {
+  zeroBefore <- if(monitorID < 10) {
+    "00"
+  }
+  else if(monitorID < 100) {
+    "0"
+  }
+  else {
+    ""
+  }
+  filename <- paste(zeroBefore, monitorID, ".csv", sep = "")
+  read.csv(paste(directory, filename, sep = "/"), comment.char = "")
 }
